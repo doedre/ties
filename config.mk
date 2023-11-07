79691818b588d9681cf499b65a240572bb5242e3
Anton Shevelyov <Anton.Shevelyov@hotmail.com>
2023-11-07 16:13:28 +0600

    Switch to clang

--- config.mk
+++ config.mk
@@ -1,16 +1,25 @@
 # Install configuration
 
-# Compilation configuration
-CXX := musl-gcc
-COMPILE_ARGS := -std=c++20 -nodefaultlibs -fno-rtti -fno-exceptions -fPIC -fconcepts -fmodules-ts -fcoroutines -fconcepts-diagnostics-depth=2 -xc++ -lc
+### Target options
 ARCH := x86_64
 
-# Source locations
+### Source locations
 SRC := src
 INC := arch/$(ARCH)
 TEST := tests
 
-# Artifacts
+### Artifacts
 OBJ := .obj
 BIN := bin
+
+### Compilation options
+CXX := clang++
+PRECOMPILE_ARGS := --precompile -fprebuilt-module-path=./$(OBJ)
+COMPILE_ARGS := -std=c++20 \
+		-nodefaultlibs \
+		-fno-rtti \
+		-fno-exceptions \
+		-fPIC \
+		-xc++ \
+		$(PRECOMPILE_ARGS)
 
